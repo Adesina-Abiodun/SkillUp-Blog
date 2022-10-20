5f5805725408f5aa1623cb8dea18b6bed51c3a5a
Christian <christianugoo@gmail.com>
2022-10-20 21:46:34 +0100

    push by Chris and Josh

--- SKILLUP-BLOG/src/LoginPage/Login.jsx
+++ SKILLUP-BLOG/src/LoginPage/Login.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { useForm } from "react-hook-form"
+import { NavLink } from "react-router-dom"
+import './Login.css'
 
 
 function Login() {
@@ -10,25 +12,25 @@ function Login() {
 
   return (
     <div className='loginpage'>
-      <NavLink to="/">SkillUp Africa Blog</NavLink>
       <div className="banner-text">
-        <h2>The Ancient city of Rome and the history of the great city.</h2>
-        <span>
+        <NavLink className="home" to="/">SkillUp Africa Blog</NavLink>
+        <h2 className="title">The Ancient city of Rome and the history of the great city.</h2>
+        <span id='autor'>
           <h3>Olive Junior</h3>
           <h4>Lead Writer, SkillUp Africa</h4>
         </span>
       </div>
       <div className='form'>
-          <h1>Welcome Back!</h1>
-          <p>Welcome back, please enter your details</p>
-          <form action="">
-          <button>Sign in with Google</button>
-          <input type="email" placeholder='Email Address'/>
-          <input type="password" placeholder='Password' />
-          <p>Forget Password</p>
-          <button>Login</button>
-          </form>
-          <p>Not registered yet? Create an Account</p> 
+        <h1>Welcome Back!</h1>
+        <p>Welcome back, please enter your details</p>
+        <form action="">
+        <button>Sign in with Google</button>
+        <input type="email" placeholder='Email Address'/>
+        <input type="password" placeholder='Password' />
+        <p>Forget Password</p>
+        <button>Login</button>
+        </form>
+        <p>Not registered yet? Create an Account</p> 
       </div>
     </div>
   )
