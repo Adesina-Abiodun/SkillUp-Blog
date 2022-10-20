e84073aecd77c395d900c2e1c5232d5c5d9a5e13
Your Name <isahworks@gmail.com>
2022-10-20 16:16:12 +0100

    Zainab finished Nav ui

--- SKILLUP-BLOG/src/NavBar/NavBar.jsx
+++ SKILLUP-BLOG/src/NavBar/NavBar.jsx
@@ -1,6 +1,50 @@
+import './NavBar.css'
+import {NavLink, Link} from 'react-router-dom';
+import { useState } from 'react';
+import fsvg from './images/facebook.svg'
+import tsvg from './images/twitter.svg'
+import isvg from './images/instagram.svg'
+import ysvg from './images/youtube.svg'
+
+
 const NavBar = () => {
+
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    const loginStat = ()=>{
+
+    }
+
     return ( 
-        <div>NavBar</div>
+        <div className='navbar'>
+        <header>
+            <h3>SkillupAfrica Blog</h3>
+        </header>
+
+        <nav className="nav">
+            <div className="nav-list">
+                <ul>
+                    <NavLink to={'/'}><li>Home</li></NavLink>
+                    <NavLink to={'/post/:id'}><li>Articles</li></NavLink>
+                    <NavLink to={'/editor/:id'}><li>About us</li></NavLink>
+                </ul>
+            </div>
+
+            <div className="clicks">
+                <div className="icons">
+                    <Link to={"https://www.facebook.com/skillupafrica1"} target="_blank"> <img src={fsvg} alt="" /></Link>
+                    <Link to={''} target="_blank"><img src={tsvg} alt="" /></Link>
+                    <Link to={'https://www.instagram.com/_skillupafrica/'} target="_blank"><img src={isvg} alt="" /></Link>
+                    <Link to={''} target="_blank"><img src={ysvg} alt="" /></Link>
+                </div>
+
+                <div className="post-button">
+                    <button onClick={loginStat}>Post your article</button>
+                </div>
+               
+            </div>
+        </nav>
+    </div>
      );
 }
  
