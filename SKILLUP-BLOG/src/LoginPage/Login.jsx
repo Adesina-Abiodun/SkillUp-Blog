77e3c6fc1f8bc8367ae3980d109feb29d2e81b23
Christian <christianugoo@gmail.com>
2022-10-20 23:41:28 +0100

    chris and josh push

--- SKILLUP-BLOG/src/LoginPage/Login.jsx
+++ SKILLUP-BLOG/src/LoginPage/Login.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import { useForm } from "react-hook-form"
 import { NavLink } from "react-router-dom"
 import './Login.css'
+import v1 from './img/v1.svg'
+import v2 from './img/v1.svg'
+import NavBar from '../NavBar/NavBar'
 
 
 function Login() {
@@ -12,27 +15,29 @@ function Login() {
 
   return (
     <div className='loginpage'>
-      <div className="banner-text">
+      <div className="txtcnt">
+      <NavBar/>
         <NavLink className="home" to="/">SkillUp Africa Blog</NavLink>
         <h2 className="title">The Ancient city of Rome and the history of the great city.</h2>
-        <span id='autor'>
-          <h3>Olive Junior</h3>
-          <h4>Lead Writer, SkillUp Africa</h4>
-        </span>
+        <div id='autor'>
+          <h3 id='olive'>Olive Junior</h3>
+          <h4 id='lead'>Lead Writer, SkillUp Africa</h4>
+        </div>
       </div>
       <div className='form'>
         <h1>Welcome Back!</h1>
         <p>Welcome back, please enter your details</p>
-        <form action="">
-        <button>Sign in with Google</button><br/><br/>
-        <p></p>
-        <input type="email" placeholder='Email Address'/>
-        <p></p>
-        <br/>
-        <input type="password" placeholder='Password' />
-        <p></p>
-        <p>Forget Password</p>
-        <button>Login</button>
+        <form className='fom' action="">
+          <button>Sign in with Google</button>
+          <br/><br/>
+          <p><img src={v1}/>or<img src={v2}/></p>
+          <input type="email" placeholder='Email Address'/>
+          <p><img src={v2}/></p>
+          <br/>
+          <input type="password" placeholder='Password' />
+          <p><img src={v2}/></p>
+          <p id='pswrd'><NavLink>Forget Password</NavLink></p>
+          <button id='login'>Login</button>
         </form>
         <p>Not registered yet? Create an Account</p> 
       </div>
