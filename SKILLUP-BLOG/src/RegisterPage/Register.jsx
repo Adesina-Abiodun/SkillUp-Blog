a5b7f97face952f92bab3902b9629fc33788d416
Idris Muzzammil Ayobami <idrisking759@gmail.com>
2022-10-20 14:45:10 +0100

    idris

--- SKILLUP-BLOG/src/RegisterPage/Register.jsx
+++ SKILLUP-BLOG/src/RegisterPage/Register.jsx
@@ -1,7 +1,28 @@
+import "./register.css"
+import { NavLink } from "react-router-dom"
+
 const Register = () => {
-    return ( 
-        <div>SignUp Page</div>
-     );
+  return (
+    <section className="register-wrapper">
+
+      <div className="register-banner">
+        <NavLink to="/">SkillUp Africa Blog</NavLink>
+
+        <div className="banner-text">
+          <h2>The Ancient city of Rome and the history of the great city.</h2>
+          <span>
+            <h3>Olive Junior</h3>
+            <h4>Lead Writer, SkillUp Africa</h4>
+          </span>
+        </div>
+      </div>
+
+      <div className="register-form">
+        <h1>Form</h1>
+      </div>
+
+    </section>
+  );
 }
- 
+
 export default Register;
