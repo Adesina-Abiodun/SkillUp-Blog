c982c1b79eb63f97ab020cbf5366daa1327f8286
Faith <faithabraham00@gmail.com>
2022-10-20 20:19:36 +0100

    The ui design for the editors page created

--- SKILLUP-BLOG/src/App.jsx
+++ SKILLUP-BLOG/src/App.jsx
@@ -1,32 +1,24 @@
-import {Routes, Route} from 'react-router-dom'
-import Home from './Home/Home'
-import Article from './ArticlePage/Article'
-import AddPost from './AddPostPage/AddPost'
-import EditorPost from './EditorPage/EditorPost'
-import Login from './LoginPage/Login'
-import Register from './RegisterPage/Register'
-
-
-
-
-
+import { Routes, Route } from "react-router-dom";
+import Home from "./Home/Home";
+import Article from "./ArticlePage/Article";
+import AddPost from "./AddPostPage/AddPost";
+import EditorPost from "./EditorPage/EditorPost";
+import Login from "./LoginPage/Login";
+import Register from "./RegisterPage/Register";
 
 function App() {
-
   return (
     <div className="App">
       <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/post/:id' element={<Article/>}/>
-        <Route path='/add-post' element={<AddPost/>}/>
-        <Route path='/editor/:id' element={<EditorPost/>}/>
-        <Route path='/login' element={<Login/>}/>
-        <Route path='/register' element={<Register/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/post/:id" element={<Article />} />
+        <Route path="/add-post" element={<AddPost />} />
+        <Route path="/editor/:id" element={<EditorPost />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
-      
-     
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
