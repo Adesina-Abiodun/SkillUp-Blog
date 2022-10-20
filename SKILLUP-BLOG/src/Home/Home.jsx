4450d2ff27f7047cae87a8556b88a75933a33862
Coolestboy01 <104928410+Coolestboy01@users.noreply.github.com>
2022-10-20 17:47:41 +0100

    new update on the home page

--- SKILLUP-BLOG/src/Home/Home.jsx
+++ SKILLUP-BLOG/src/Home/Home.jsx
@@ -1,11 +1,40 @@
 import NavBar from "../NavBar/NavBar";
+import { useEffect, useState } from "react";
+import axios from 'axios';
+import moment from 'moment';
+import './Home.css'
 
 const Home = () => {
+    const [posts, setposts] = useState([]);
+    useEffect(() => {
+        axios.get('https://jsonplaceholder.typicode.com/photos')
+        .then(response => {
+            console.log(response.data);
+            setposts(response.data)
+        })
+    })
+
     return ( 
         <div>
             <NavBar/>            
-            HOME PAGE</div>
-     );
+            <h1 className="header">Latest Articles</h1>
+            
+            <div className="blogPost">
+                {posts.map((posts) => {
+                    return(
+                        <div className="blog-container" key={posts._id}>
+                            <div className="articleImage"><img src={posts.url} alt="" /></div>
+                            <p>{moment(posts.date).format('Do MMMM YYYY, h:mm:ss a')}</p>
+                            <h3>{posts.title}</h3>
+                            <p>{posts.thumbnailUrl}</p>
+                        </div>
+                    )
+                
+                })}
+            </div>
+            <button className="homeBtn" type="submit">Load more articles</button>
+        </div>
+    );
 }
  
 export default Home;
