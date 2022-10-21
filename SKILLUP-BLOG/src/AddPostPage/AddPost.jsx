b5d228f3b46be2bbe005dc74fb98d39f6605849e
Kingsley Igbor <chidoziek2@gmail.com>
2022-10-21 01:07:45 +0100

    commit

--- SKILLUP-BLOG/src/AddPostPage/AddPost.jsx
+++ SKILLUP-BLOG/src/AddPostPage/AddPost.jsx
@@ -1,54 +1,62 @@
 import NavBar from "../NavBar/NavBar";
 import { useForm } from "react-hook-form";
 import "./AddPost.css";
+import axios from "axios"
 
 const AddPost = () => {
-  const { register, handleSubmit } = useForm();
+    const { register, handleSubmit } = useForm();
 
-  const submitData = () => {
-    console.log("Success");
-  };
-  return (
-    <div>
-      <NavBar />
 
-      <div className="Blog-article">
+    const onSubmit = async (data) => {
+      axios
+      .post("https://dashboard.heroku.com/apps/miniblogskillup", data)
+      .then((response)=>{
+        alert(response.data.message)
+      })
+    };
+
+
+    return (
         <div>
-          <h2>Post a new article</h2>
-          <br />
-          <form onSubmit={handleSubmit(submitData)}>
-            <input
-              type="text"
-              {...register("title")}
-              placeholder="Blog title"
-              className="blog-title"
-            />{" "}
-            <br />
-            <br />
-            <input
-              type="text"
-              {...register("Image url")}
-              placeholder="Image url"
-              className="image-url"
-            />
-            <br />
-            <br />
-            <textarea
-              cols="30"
-              rows="10"
-              {...register("body")}
-              placeholder="Blog Content"
-              className="blog-content"
-            ></textarea>
-            <br />
-            <button type="submit" className="post">
-              Post
-            </button>
-          </form>
+            <NavBar />
+
+            <div className="Blog-article">
+                <div>
+                    <h2>Post a new article</h2>
+                    <br />
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <input
+                            type="text"
+                            {...register("title")}
+                            placeholder="Blog title"
+                            className="blog-title"
+                        />{" "}
+                        <br />
+                        <br />
+                        <input
+                            type="text"
+                            {...register("Image url")}
+                            placeholder="Image url"
+                            className="image-url"
+                        />
+                        <br />
+                        <br />
+                        <textarea
+                            cols="30"
+                            rows="10"
+                            {...register("body")}
+                            placeholder="Blog Content"
+                            className="blog-content"
+                        ></textarea>
+                        <br />
+                        <button type="submit" className="post">
+                            Post
+                        </button>
+                    </form>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default AddPost;
