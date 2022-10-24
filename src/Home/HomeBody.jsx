5009ae6af5431baa4b54e36d3fb33604b24f5b8c
Coolestboy01 <104928410+Coolestboy01@users.noreply.github.com>
2022-10-24 21:42:29 +0100

    update on home

--- src/Home/HomeBody.jsx
+++ src/Home/HomeBody.jsx
@@ -5,20 +5,12 @@ import './HomeBody.css'
 import { Link } from "react-router-dom";
 
 const Home = () => {
-    const [posts, setposts] = useState({
-        token: "",
-        isLoggedIn: true
-    });
+    const [posts, setposts] = useState([]);
     useEffect(() => {
-        axios.get('https://jsonplaceholder.typicode.com/photos', {
-            headers: {
-                "content-type": "application/json",
-                "Authorization": access.token,
-            },
-        })
+        axios.get('https://miniblogskillup.herokuapp.com/api/posts/')
         .then(response => {
             console.log(response.data);
-            setposts(response.data)
+            setposts(response.data.data.news)
         })
     })
 
@@ -26,14 +18,14 @@ const Home = () => {
         <div>
             <h1 className="home-article-header">Latest Articles</h1>
             <div className="home-blogpost">
-                {posts?.slice(0,9).map(({id, url, date, title}) => {
+                {posts?.slice(0,9).map(({_id, coverImage, createdAt, title, body}) => {
                     return(
-                        <Link className='home-link' to={`/posts/${id}`}>
-                            <div className="home-blogcontainer" key={id}>
-                                <div ><img className="home-articleimage" src={url} alt="" /></div>
-                                <p className="home-date">{moment(date).startOf('hour').fromNow()}</p>
+                        <Link className='home-link' to={`posts/${_id}`} key={_id}>
+                            <div className="home-blogcontainer" key={_id}>
+                                <div ><img className="home-articleimage" src={coverImage} alt={title} /></div>
+                                <p className="home-date">{moment(createdAt).startOf('hour').fromNow()}</p>
                                 <h4 className="home-articletitle">{title}</h4>
-                                <p className="home-mainarticle">{title.slice(0,60)}....</p>
+                                <p className="home-mainarticle">{body.slice(0,60)}....</p>
                             </div>
                         </Link>
                     )
