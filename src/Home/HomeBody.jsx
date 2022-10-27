d7b83baa7cd185c1dd0cd1893f4e6e42c3870357
topeisah <110097953+topeisah@users.noreply.github.com>
2022-10-27 01:11:45 +0100

    Revert "Home branch"

--- src/Home/HomeBody.jsx
+++ src/Home/HomeBody.jsx
@@ -7,12 +7,12 @@ import { Link } from "react-router-dom";
 const Home = () => {
     const [posts, setposts] = useState([]);
     useEffect(() => {
-        axios.get('https://jsonplaceholder.typicode.com/photos')
-        .then(response => {
+    axios
+      .get("https://miniblogskillup.herokuapp.com/api/posts/")
+      .then((response) => {
             console.log(response.data);
-            setposts(response.data)
-        })
-    })
+        setposts(response.data.data.news.reverse(a => a));
+    }), []});
 
     return ( 
         <div>
